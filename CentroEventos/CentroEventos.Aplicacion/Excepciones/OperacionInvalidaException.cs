265cc438857c80d257093fc119fd1ef7aebbdb83
GabiJLara <gabijlara@gmail.com>
2025-05-16 02:08:15 -0300

    Create OperacionInvalidaException.cs

---
+++ CentroEventos/CentroEventos.Aplicacion/Excepciones/OperacionInvalidaException.cs
@@ -0,0 +1,8 @@
+public class OperacionInvalidaException : Exception {
+
+    public OperacionInvalidaException() { }
+
+    public OperacionInvalidaException(string message): base(message) { }
+
+    public OperacionInvalidaException(string message,Exception inner): base(message, inner) { }
+}
