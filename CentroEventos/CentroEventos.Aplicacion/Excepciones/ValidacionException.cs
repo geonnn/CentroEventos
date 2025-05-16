d1c838ef6b4a3242eb878ab1c1eaab6ccd9c89f7
GabiJLara <gabijlara@gmail.com>
2025-05-16 02:03:01 -0300

    Create ValidacionException.cs

---
+++ CentroEventos/CentroEventos.Aplicacion/Excepciones/ValidacionException.cs
@@ -0,0 +1,8 @@
+public class ValidacionException : Exception {
+
+    public ValidacionException() { }
+
+    public ValidacionException(string message): base(message) { }
+
+    public ValidacionException(string message,Exception inner): base(message, inner) { }
+}
