430ca568a51675dd20b3fdfbd1de7c84f478e54e
GabiJLara <gabijlara@gmail.com>
2025-05-16 02:04:34 -0300

    Create EntidadNotFoundException.cs

---
+++ CentroEventos/CentroEventos.Aplicacion/Excepciones/EntidadNotFoundException.cs
@@ -0,0 +1,8 @@
+public class EntidadNotFoundException : Exception {
+
+    public EntidadNotFoundException() { }
+
+    public EntidadNotFoundException(string message): base(message) { }
+
+    public EntidadNotFoundException(string message,Exception inner): base(message, inner) { }
+}
