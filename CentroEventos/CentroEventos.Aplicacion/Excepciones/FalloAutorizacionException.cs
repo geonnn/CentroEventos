9ea6c14892dec4f1ffb0de3b89bf8ce9b5d5da64
GabiJLara <gabijlara@gmail.com>
2025-05-16 01:59:23 -0300

    Create FalloAutorizacionException.cs

---
+++ CentroEventos/CentroEventos.Aplicacion/Excepciones/FalloAutorizacionException.cs
@@ -0,0 +1,8 @@
+public class FalloAutorizacionException: : Exception {
+    
+    public FalloAutorizacionException:() { }
+
+    public FalloAutorizacionException:(string message): base(message) { }
+
+    public FalloAutorizacionException:(string message,Exception inner): base(message, inner) { }
+}
