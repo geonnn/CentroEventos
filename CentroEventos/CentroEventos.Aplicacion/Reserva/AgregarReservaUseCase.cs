0bb3753759c3a455cfd192d3a368a9b1c38f309f
Gabriel <gracielabalbuena@OrdenadGraciela.fibertel.com.ar>
2025-05-16 05:45:38 -0300

    holas

--- CentroEventos/CentroEventos.Aplicacion/Reserva/AgregarReservaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/Reserva/AgregarReservaUseCase.cs
@@ -9,6 +9,9 @@ public class AgregarReservaUseCase(IRepositorioReserva repoReserva, ReservaValid
             if (!validador.Validar(reserva, out string mensajeError))
                 throw new ValidacionException(mensajeError);
 
+            //if(!TienePermiso) Validar que tenga permiso el usuario
+            //    throw new FalloAutorizacionException("No tiene permiso para añadir una persona");
+
             repoReserva.AgregarReserva(reserva);
             Console.WriteLine($"Persona ID {reserva.Id} añadido exitosamente.");
         }
@@ -17,5 +20,12 @@ public class AgregarReservaUseCase(IRepositorioReserva repoReserva, ReservaValid
             Console.WriteLine("Excepción: " + e);
             Console.WriteLine($"Error al añadir el reserva ID {reserva.Id}.");
         }
+        catch(FalloAutorizacionException e)
+        {
+            Console.WriteLine("Excepción: " + e);
+            Console.WriteLine($"Error al añadir reserva {reserva.Id}.");
+        }
+        
+        
     }
 }
