d0f8685b63ae0eb515992ffd5825058d55ba33d4
GabiJLara <gabijlara@gmail.com>
2025-05-16 02:05:48 -0300

    Create CupoExcedidoException.cs

---
+++ CentroEventos/CentroEventos.Aplicacion/Excepciones/CupoExcedidoException.cs
@@ -0,0 +1,8 @@
+public class CupoExcedidoException : Exception {
+
+    public CupoExcedidoException() { }
+
+    public CupoExcedidoException(string message): base(message) { }
+
+    public CupoExcedidoException(string message,Exception inner): base(message, inner) { }
+}
